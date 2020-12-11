70f374f4206cdc8b33e1a3577513de68bdd3356b
demetrios dolios <dolios@gmail.com>
2020-12-11 15:44:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var formField = document.querySelector('form');
 var groceryItem = document.querySelector('ul');
 var buttonClearAll = document.querySelector('#clear-list');
 var inputGroceryItem =document.getElementById('item');
+var myNodeList = document.getElementsByTagName('li');
+
+
 
 //var itemsArray = [];
-var itemsArray = localStorage.getItem('groceryItems') ? JSON.parse(localStorage.getItem('groceryItems')) :[];
+var itemsArray = localStorage.getItem('groceryItems')?JSON.parse(localStorage.getItem('groceryItems')):[];
 
 localStorage.setItem('groceryItems',JSON.stringify(itemsArray));
 
@@ -13,19 +16,25 @@ var storage = JSON.parse(localStorage.getItem('groceryItems'));
 
 formField.addEventListener('submit', function(e) {
     e.preventDefault();
+    
+//console.log("from form"+inputGroceryItem.value);
 
     itemsArray.push(inputGroceryItem.value);
     localStorage.setItem('groceryItems',JSON.stringify(itemsArray));
 
     addGroceryList(inputGroceryItem.value);
     inputGroceryItem.value='';
-    
+   
+        
 });
 
 
 var addGroceryList = function(text) {
 
     var addToList = document.createElement('li');
+    var children = groceryItem.children.length+1;
+    //addToList.setAttribute('class','checked'/*+children*/);
+
     addToList.textContent=text;
     groceryItem.appendChild(addToList);
 
@@ -36,6 +45,7 @@ for (var i=0; i< storage.length; i++) {
     addGroceryList(storage[i]);
 }
 
+
 buttonClearAll.addEventListener('click',function() {
 
     localStorage.clear();
@@ -44,3 +54,20 @@ buttonClearAll.addEventListener('click',function() {
 
     }
 });
+
+
+groceryItem.addEventListener('click',function(e) {
+
+   
+    console.log(e.target);
+        
+if (e.target.tagName==='LI') {
+        
+    
+    e.target.classList.toggle('checked');
+    } 
+    
+    
+});
+
+
